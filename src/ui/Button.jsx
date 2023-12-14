a30a8262095712d5baec92351d9c64e58f3d12f7
Jose Copeti <jrcopeti@gmail.com>
2023-12-14 15:58:23 +0100

    styled components

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,18 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  border: none;
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+  ${(props => sizes[props.size])}
+  ${(props => variations[props.variation])}
+  `;
+
+Button.defaultProps = {
+  size: "medium",
+  variation: "primary",
+};
+
+export default Button;
