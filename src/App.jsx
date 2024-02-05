590661a56064006fccf031e249149a9ac7eb1aea
Jose Copeti <jrcopeti@gmail.com>
2024-02-05 18:52:44 +0100

    booking details

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import AppLayout from "./ui/AppLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+import Booking from "./pages/Booking";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -41,6 +42,10 @@ const router = createBrowserRouter([
       {
         path: "bookings",
         element: <Bookings />,
+      },
+      {
+        path: "bookings/:bookingId",
+        element: <Booking />,
       },
       {
         path: "Cabins",
