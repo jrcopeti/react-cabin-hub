9966b578ab0d80b58a69f4fc7a2737ccff61e5e1
Jose Copeti <jrcopeti@gmail.com>
2024-02-12 14:00:11 +0100

    fix bugs

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -7,6 +7,7 @@ import Tag from "../../ui/Tag";
 import ButtonGroup from "../../ui/ButtonGroup";
 import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
+import Empty from "../../ui/Empty";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
@@ -33,6 +34,7 @@ function BookingDetail() {
   const moveBack = useMoveBack();
 
   if (isLoading) return <Spinner />;
+  if(!booking) return <Empty resourceName="booking"/>;
 
   const { status, id: bookingId } = booking;
 
