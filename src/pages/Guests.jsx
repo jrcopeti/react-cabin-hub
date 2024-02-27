bae7947c703bba56796bc03a7c1a6d8d1aa53317
Jose Copeti <jrcopeti@gmail.com>
2024-02-27 12:44:18 +0100

    create guests form

--- src/pages/Guests.jsx
+++ src/pages/Guests.jsx
@@ -1,24 +1,23 @@
-import AddCabin from "../features/cabins/AddCabin"
-import CabinTable from "../features/cabins/CabinTable"
-import CabinTableOperations from "../features/cabins/CabinTableOperations"
-import GuestTable from "../features/guests/GuestTable"
-import GuestTableOperations from "../features/guests/GuestTableOperation"
-import Heading from "../ui/Heading"
-import Row from "../ui/Row"
+import AddGuest from "../features/guests/AddGuest";
+import GuestTable from "../features/guests/GuestTable";
+import GuestTableOperations from "../features/guests/GuestTableOperation";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
 
 function Guest() {
   return (
     <>
-    <Row type="horizontal">
-      <Heading as="h1">All Guests</Heading>
-      <GuestTableOperations/>
-    </Row>
+      <Row type="horizontal">
+        <Heading as="h1">All Guests</Heading>
+        <GuestTableOperations />
+        <AddGuest />
+      </Row>
 
-    <Row>
-    <GuestTable/>
-    </Row>
-  </>
-  )
+      <Row>
+        <GuestTable />
+      </Row>
+    </>
+  );
 }
 
-export default Guest
+export default Guest;
