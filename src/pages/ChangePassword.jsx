7409fdf89a285c9e9ce99eebde8d36e898261829
Jose Copeti <jrcopeti@gmail.com>
2024-03-14 13:54:13 +0100

    responsive design

--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -6,14 +6,14 @@ import ChangePasswordForm from "../features/authentication/ChangePasswordForm";
 import { screenSizes } from "../utils/constants";
 
 const LoginLayout = styled.main`
-  min-height: 100vh;
+  min-height: 80dvh;
   display: grid;
   grid-template-columns: 52rem;
   align-content: center;
   justify-content: center;
   gap: 3.2rem;
   background-color: var(--color-grey-50);
-  
+
   @media (max-width: ${screenSizes.tablet}) {
     grid-template-columns: 1fr;
     padding: 1rem 1.8rem 3.2rem;
