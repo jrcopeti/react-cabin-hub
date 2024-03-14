f80a5b04a82ed922a8d9346e0ba133b32a02c971
Jose Copeti <jrcopeti@gmail.com>
2024-03-14 11:38:57 +0100

    responsive design

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -4,10 +4,10 @@ import Row from "../ui/Row";
 
 function Settings() {
   return (
-    <Row>
+    <>
       <Heading as="h1">Update hotel settings</Heading>
       <UpdateSettingsForm />
-    </Row>
+    </>
   );
 }
 
