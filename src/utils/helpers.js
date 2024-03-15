1d7fabc50c5320aa737e166e47c7cb1c263dcd73
Jose Copeti <jrcopeti@gmail.com>
2024-03-15 18:36:36 +0100

    responsive design

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,7 @@
-import { formatDistance, parseISO } from "date-fns";
+import { add, formatDistance, parseISO } from "date-fns";
 import { differenceInDays } from "date-fns";
 
-// This function work for both Date objects and strings (which come from Supabase)
+// This function work for both Date objects and strings 
 export const subtractDates = (dateStr1, dateStr2) =>
   differenceInDays(parseISO(String(dateStr1)), parseISO(String(dateStr2)));
 
@@ -12,11 +12,10 @@ export const formatDistanceFromNow = (dateStr) =>
     .replace("about ", "")
     .replace("in", "In");
 
-// Supabase needs an ISO date string. However, that string will be different on every render because the ms or sec have changed.
+// Supabase needs an ISO date string
 export const getToday = function (options = {}) {
   const today = new Date();
 
-  // This is necessary to compare with created_at from Supabase, because it's not at 0.0.0.0, needs to be set the date to the END of the day when compare it with earlier dates
   if (options?.end)
     // Set to the last second of the day
     today.setUTCHours(23, 59, 59, 999);
@@ -25,7 +24,7 @@ export const getToday = function (options = {}) {
 };
 
 export function toLocalISODate(date) {
-  const timeZoneOffset = new Date().getTimezoneOffset() * 60000; 
+  const timeZoneOffset = new Date().getTimezoneOffset() * 60000;
   return new Date(new Date(date) - timeZoneOffset).toISOString().slice(0, 10);
 }
 
@@ -33,3 +32,9 @@ export const formatCurrency = (value) =>
   new Intl.NumberFormat("en", { style: "currency", currency: "USD" }).format(
     value
   );
+
+  export function fromToday(numDays, withTime = false) {
+    const date = add(new Date(), { days: numDays });
+    if (!withTime) date.setUTCHours(0, 0, 0, 0);
+    return date.toISOString().slice(0, -1);
+  }
