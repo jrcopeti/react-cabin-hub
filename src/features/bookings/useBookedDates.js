5bf2dc7db6ec88af721df7bfa55431aa0610b532
Jose Copeti <jrcopeti@gmail.com>
2024-03-17 15:32:52 +0100

    implement date picker

--- src/features/bookings/useBookedDates.js
+++
@@ -1,9 +0,0 @@
-// import { useEffect } from "react";
-// import { useGetBookingsByCabin } from "./useBookingsByCabin";
-
-// export function useBookedDates(cabinId) {
-//   const {isLoading, bookings: bookedDates, error} = useGetBookingsByCabin(cabinId);
-
-//   useEffect
-
-// }
